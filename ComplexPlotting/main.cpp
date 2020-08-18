69f97c86216d37749e817e64cdab1782c06a83a4
Robert <robert.trololo@gmail.com>
2020-08-18 14:11:30 +0200

    Included Graphics libraries

--- ComplexPlotting/main.cpp
+++ ComplexPlotting/main.cpp
@@ -1,7 +1,21 @@
+#include <stdio.h>
+#include <SDL.h>
 
+#undef main
+
+void PrintSDLVersion()
+{
+	SDL_version* v = new SDL_version;
+	SDL_VERSION(v);
+	printf("Running on SDL %i.%i.%i\n", v->major, v->minor, v->patch);
+	
+	delete v;
+	v = nullptr;
+}
 
 int main(int argc, char** argv)
 {
-
+	PrintSDLVersion();
+	
 	return 0;
 }
